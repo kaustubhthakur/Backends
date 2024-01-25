0c1788c03494ae625dd9714f59a558c69d8cb64d
Kaustubh <kaustubhthakur76@gmail.com>
2024-01-25 21:58:02 +0530

    jobboard-backend

--- social-network/routes/friends.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const {addFriend,removeFriend} = require('../controllers/friends');
-router.post('/:id',addFriend);
-router.delete('/:id',removeFriend);
-module.exports = router;
--- social-network/routes/votes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const {upVotes,downvotes} = require('../controllers/votes');
-router.post('/:id',upVotes);
-router.put('/:id',downvotes);
-module.exports = router;
